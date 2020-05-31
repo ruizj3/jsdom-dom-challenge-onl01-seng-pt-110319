1c46335f53e513e32de03bcf06d4eb131a642e65
ruizj3 <ruizj3@uw.edu>
2020-05-30 20:20:42 -0700

    Update challenge.js

--- js/challenge.js
+++ js/challenge.js
@@ -1,24 +1,74 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const comment = new Comment();
+  const comments = document.getElementById("list");
+	const commentForm = document.getElementById("comment-form");
+	const commentInput = document.getElementById("comment-input");
 
-  const newCommentForm = document.getElementById("comment-form");
-  const newComment = document.getElementById("comment-input");
+  const startInterval = setInterval(incrementPerSecond, 1000);
+	const counter = document.getElementById("counter");
+	const minus = document.getElementById("minus");
+	const plus = document.getElementById("plus");
+	const heart = document.getElementById("heart");
+	const pause = document.getElementById("pause");
+	const submit = document.getElementById("submit");
+	const likes = document.getElementsByClassName("likes")[0];
 
-  const commentUl = document.getElementById("list");
+	minus.addEventListener("click", decrement);
+	plus.addEventListener("click", increment);
+	pause.addEventListener("click", pauseButtonClicked);
+	heart.addEventListener("click", likeButtonClicked);
 
-  const renderApp = () => (commentUl.innerHTML = Comment.renderComment());
+	commentForm.addEventListener("submit", function (event) {
+		let comment = document.createElement("p");
+		comment.innerText = commentInput.value;
 
-  newCommentForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    Comment.createNewComment(newComment.value);
-    e.target.reset();
-    renderApp();
-  });
+		event.preventDefault();
+		comments.appendChild(comment);
+	});
 
-  commentUl.addEventListener("click", (e) => {
-    if (e.target.nodeName === "BUTTON") {
-      Comment.deleteComment(e.target.dataset.description);
-      renderApp();
-    }
-  });
+	function incrementPerSecond() {
+		if (pause.innerText == "pause") {
+			counter.innerHTML++;
+		} else {
+			clearInterval(startInterval);
+		}
+	}
+
+	function increment() {
+		counter.innerHTML++;
+	}
+
+	function decrement() {
+		counter.innerHTML--;
+	}
+
+	function pauseButtonClicked() {
+		if (pause.innerText == "pause") {
+			pause.innerText = "resume";
+
+			submit.disabled = true;
+			heart.disabled = true;
+			plus.disabled = true;
+			minus.disabled = true;
+
+			clearInterval(startInterval);
+		} else {
+			pause.innerText = "pause";
+
+			submit.disabled = false;
+			heart.disabled = false;
+			plus.disabled = false;
+			minus.disabled = false;
+
+			startInterval = setInterval(incrementPerSecond, 1000);
+		}
+	}
+
+	function likeButtonClicked() {
+		let like = document.createElement("li");
+		like.innerText = counter.innerHTML + " was liked";
+
+		likes.append(like);
+	}
+
+	startInterval;
 });
